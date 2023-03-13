14abff8282ceeac68941d74edb08d7eb4afbb34d
melany estevez camata <elibialany@gmail.com>
2023-03-13 02:58:56 -0400

    feat: add photho

--- onetomany/src/photo/photo.module.ts
+++ onetomany/src/photo/photo.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
-
-@Module({})
-export class PhotoModule {}
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Profile } from 'src/entity/profiles.entity';
+import { PhotoController } from './photo.controller';
+import { PhotoService } from './photo.service';
+import { Photo } from 'src/entity/photos.entity';
+@Module({
+  imports: [TypeOrmModule.forFeature([Photo,Profile])],
+  controllers: [PhotoController],
+  providers: [PhotoService],
+})
+export class ProfileModule {}
--- onetomany/src/photo/photo.service.ts
+++ onetomany/src/photo/photo.service.ts
@@ -1,33 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreatePhotoDTO } from 'src/dto/photos.dto';
 import { Photo } from 'src/entity/photos.entity';
 import { Repository } from 'typeorm';
-
+import { Profile} from 'src/entity/profiles.entity'
 @Injectable()
 export class PhotoService {
   constructor(
     @InjectRepository(Photo)
     private photoRepository: Repository<Photo>,
+    @InjectRepository(Profile)
+    private profileRepository: Repository<Profile>,
   ) {}
 
-  public async getAllPhotos(): Promise<Photo[]> {
+  // public async getAllPhotos(): Promise<Photo[]> {
+  //   return this.photoRepository.find();
+  // }
+
+  // public async getPhotoById(id: string): Promise<Photo> {
+  //   const photo = this.photoRepository.findOneBy({ id });
+  //   if (!photo) throw new Error('Photo not found');
+  //   return photo;
+  // }
+
+  // // public async deletePhotoById(uuid: string): Promise<void> {
+  // //   await this.photoRepository.delete(uuid);
+  // // }
+
+  // public async updatePhoto(photo: CreatePhotoDTO): Promise<Photo> {
+  //   return this.photoRepository.save(photo);
+  // }
+  public async addPhoto(request : CreatePhotoDTO):Promise<Photo[]> {
+    const foundProfile= await this.profileRepository.findOneBy({id:request.idprofile});
+    // const profile = await this.profileRepository.findOne({id: request.idprofile});
+    console.log(foundProfile);
     return this.photoRepository.find();
+    // newPhoto.url = request .url;
+    // newPhoto.profile = profile;
+    // return this.photoRepo.save(newPhoto);
   }
-
-  public async getPhotoById(id: string): Promise<Photo> {
-    const photo = this.photoRepository.findOneBy({ id });
-    if (!photo) throw new Error('Photo not found');
-    return photo;
-  }
-
-  public async deletePhotoById(uuid: string): Promise<void> {
-    await this.photoRepository.delete(uuid);
-  }
-
-  public async postPhoto(photo: Partial<Photo>): Promise<Photo> {
-    return this.photoRepository.save(photo);
-  }
-
   //   public async putPhotoById(uuid: string, photo: Partial<Photo>) {
   //     const myPhoto = await this.getPhotoById(uuid);
   //     if (!myPhoto) throw new Error('Photo not found');
